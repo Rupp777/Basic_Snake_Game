c5be4820474c137402d83f0b7ea94666c6de996d
Rupp777 <Rchoudhari777>
2021-11-09 18:55:35 +0530

    commit
    snake motion along with the food grab code which incress the num if snake tail...

---
+++ Basic Snake Game/Assets/Scripts/Food.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public BoxCollider2D boundry;
+
+
+    private void Start()
+    {
+        Randomposition();
+    }
+
+
+    private void Update()
+    {
+    }
+
+    private void Randomposition()
+    {
+        Bounds bounds = this.boundry.bounds;
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y) , 0f);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Randomposition();
+        }
+    }
+}
