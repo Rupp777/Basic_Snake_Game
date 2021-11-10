ec115e3ef1c40f003548cbf796ee934e0f0ce0f1
Rupp777 <Rchoudhari777>
2021-11-10 21:05:43 +0530

    Commit_1
    Updated the movement function

--- Basic Snake Game/Assets/Scripts/Snake.cs
+++ Basic Snake Game/Assets/Scripts/Snake.cs
@@ -28,19 +28,19 @@ public class Snake : MonoBehaviour
         //position.x = xvalue * Time.deltaTime * 5;
         //position.y = yvalue * Time.deltaTime * 5;
         //transform.Translate(position.x, position.y , 0);
-        if(Input.GetKeyDown(KeyCode.W))
+        if(Input.GetKeyDown(KeyCode.W) && _direction != Vector2.down)
         {
             _direction = Vector2.up;
         }
-        else if(Input.GetKeyDown(KeyCode.S))
+        else if(Input.GetKeyDown(KeyCode.S) && _direction != Vector2.up)
         {
             _direction = Vector2.down;
         }
-        else if (Input.GetKeyDown(KeyCode.A))
+        else if (Input.GetKeyDown(KeyCode.A) && _direction != Vector2.right)
         {
             _direction = Vector2.left;
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKeyDown(KeyCode.D) && _direction != Vector2.left)
         {
             _direction = Vector2.right;
         }
